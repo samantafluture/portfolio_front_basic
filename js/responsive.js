c7707e9115edede49d67c192103dcb27e13ca392
Samanta Fluture <samantafluture@gmail.com>
2022-08-02 18:42:20 -0400

    create menu mobile for all pages

--- js/responsive.js
+++ js/responsive.js
@@ -1,8 +1,19 @@
 function openMenuMobile() {
-    var topNav = document.getElementById("topnav");
-    if (topNav.className === "topnav") {
-        topNav.className += " responsive";
-    } else {
-        topNav.className = "topnav";
-    }
-  }
+	const btnContainer = document.getElementById('btnContainer');
+	const nav = document.getElementById('navBar');
+
+	nav.className = 'responsive__nav';
+
+	btnContainer.innerHTML =
+		'<button id="closeBtn" type="button" onclick="closeMenuMobile()"><i class="fa-solid fa-xmark"></i></button>';
+}
+
+function closeMenuMobile() {
+    const btnContainer = document.getElementById('btnContainer');
+    const nav = document.getElementById('navBar');
+
+    nav.className = 'hidden';
+
+    btnContainer.innerHTML =
+		'<button id="openBtn" type="button" onclick="openMenuMobile()"><i class="fa fa-bars"></i></button>';
+}
